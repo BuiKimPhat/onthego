e9e9b2771182de99f409f6fc1dbdae85465eadfc
Leo <35719742+BuiKimPhat@users.noreply.github.com>
2020-09-21 00:24:36 +0700

    intro 1/2

---
+++ app/src/main/java/com/leobkdn/onthego/IntroAdapter.java
@@ -0,0 +1,65 @@
+package com.leobkdn.onthego;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+// Working with UI
+public class IntroAdapter extends RecyclerView.Adapter<IntroAdapter.IntroViewHolder> {
+    // List of introItems
+    private List<IntroItem> introItemList;
+    //constructor of adapter, passing introItemList as parameter of class
+    public IntroAdapter(List<IntroItem> introItemList) {
+        this.introItemList = introItemList;
+    }
+
+    //  managing index of indicators
+    @NonNull
+    @Override
+    public IntroViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new IntroViewHolder(
+                //Transform xml file into View in java, and maybe bind to parent
+                LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.intro_item_container, parent, false
+                )
+        );
+    }
+    // pass introItemList[position]'s data to introViewHolder
+    @Override
+    public void onBindViewHolder(@NonNull IntroViewHolder holder, int position) {
+        holder.setIntroData(introItemList.get(position));
+    }
+    //size of introItemList
+    @Override
+    public int getItemCount() {
+        return introItemList.size();
+    }
+
+    //each UI View of intro
+    class IntroViewHolder extends RecyclerView.ViewHolder {
+        private TextView introTitle;
+        private TextView introDescription;
+        private ImageView introImage;
+
+        //constructor of holder, holder attributes connect to UI View
+        IntroViewHolder(@NonNull View itemView) {
+            super(itemView);
+            introTitle = itemView.findViewById(R.id.introTitle);
+            introDescription = itemView.findViewById(R.id.introDescription);
+            introImage = itemView.findViewById(R.id.introImage);
+        }
+        //edit values in UI View
+        void setIntroData(IntroItem introItem){
+            introTitle.setText(introItem.getTitle());
+            introDescription.setText(introItem.getDescription());
+            introImage.setImageResource(introItem.getImage());
+        }
+    }
+}
--- app/src/main/java/com/leobkdn/onthego/MainActivity.java
+++ app/src/main/java/com/leobkdn/onthego/MainActivity.java
@@ -1,14 +1,105 @@
 package com.leobkdn.onthego;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private IntroAdapter introAdapter;
+    private LinearLayout introIndicators;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupIntroItems();
+        ViewPager2 introViewPager = findViewById(R.id.introViewPager);
+        introViewPager.setAdapter(introAdapter);
+
+        introIndicators = findViewById(R.id.introIndicators);
+        setupIntroIndicators();
+        setCurrentIntroIndicator(0);
+
+        introViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentIntroIndicator(position);
+            }
+        });
+    }
+
+    private void setupIntroItems() {
+        List<IntroItem> introItemList = new ArrayList<>();
+
+        IntroItem itemIntroduction = new IntroItem();
+        itemIntroduction.setTitle("Chào mừng đến với On The Go");
+        itemIntroduction.setDescription("Ứng dụng gợi ý dành cho những người thích đi du lịch");
+        itemIntroduction.setImage(R.drawable.onthego_logo);
+
+        IntroItem itemDestination = new IntroItem();
+        itemDestination.setTitle("Gợi ý điểm đến, lập lộ trình");
+        itemDestination.setDescription("Không cần phải băn khoăn nên đi đâu, tự động gợi ý chuyến đi tốt nhất");
+        itemDestination.setImage(R.drawable.destinations);
+
+        IntroItem itemFlight = new IntroItem();
+        itemFlight.setTitle("Tự động tìm chuyến bay phù hợp");
+        itemFlight.setDescription("Giúp bạn chọn những chuyến bay tốt nhất");
+        itemFlight.setImage(R.drawable.flight);
+
+        IntroItem itemMoney = new IntroItem();
+        itemMoney.setTitle("Chi tiêu hợp lí");
+        itemMoney.setDescription("Tự lập lộ trình, hoạt động du lịch theo số tiền mà bạn có");
+        itemMoney.setImage(R.drawable.money);
+
+        introItemList.add(itemIntroduction);
+        introItemList.add(itemDestination);
+        introItemList.add(itemFlight);
+        introItemList.add(itemMoney);
+
+        introAdapter = new IntroAdapter(introItemList);
+    }
+
+    private void setupIntroIndicators() {
+        ImageView[] indicators = new ImageView[introAdapter.getItemCount()];
+        //dunno
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8, 0, 8, 0);
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.intro_indicators_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            introIndicators.addView(indicators[i]);
+        }
+    }
+
+    private void setCurrentIntroIndicator(int index) {
+        int childCount = introIndicators.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) introIndicators.getChildAt(i);
+            if (i == index) {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.intro_indicator_active)
+                );
+            } else {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.intro_indicators_inactive)
+                );
+            }
+        }
     }
 }
